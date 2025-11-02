27e193f1498c40ed12cabfdea2cd5fcb3a8833a2
adamhardison284-pixel <adamhardison284@gmail.com>
2025-11-02 01:47:29 +0100

    Update test_ip.py

--- test_ip.py
+++ test_ip.py
@@ -10,20 +10,20 @@ params = {
 # Optional headers (for example, if the API needs a key or token)
 headers = {
 }
-
-try:
-    # Send GET request
-    response = requests.get(url, headers=headers, params=params)
-
-    # Check if the request was successful
-    response.raise_for_status()
-
-    # Parse JSON response
-    data = response.json()
-
-    # Print the result
-    print("✅ Success! Response data:")
-    print(data)
-
-except requests.exceptions.RequestException as e:
-    print("❌ Request failed:", e)
+for x in range(4):
+    try:
+        # Send GET request
+        response = requests.get(url, headers=headers, params=params)
+    
+        # Check if the request was successful
+        response.raise_for_status()
+    
+        # Parse JSON response
+        data = response.json()
+    
+        # Print the result
+        print("✅ Success! Response data:")
+        print(data.ip)
+    
+    except requests.exceptions.RequestException as e:
+        print("❌ Request failed:", e)
