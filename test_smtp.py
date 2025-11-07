6c851c392edc66fec7b63e86df85bf11126dfd7f
adamhardison284-pixel <adamhardison284@gmail.com>
2025-11-07 22:54:11 +0100

    Update test_smtp.py

--- test_smtp.py
+++ test_smtp.py
@@ -8,7 +8,7 @@ from zoneinfo import ZoneInfo
 def send_email(subject, sender_email, password, receiver_email, text, html, offer_id, smtp_host):
   msg = MIMEMultipart("alternative")
   msg["Subject"] = subject
-  msg["From"] = sender_email
+  msg["From"] = "no-reply@rolahgail.com"
   msg["To"] = receiver_email
   
   # Attach both versions
