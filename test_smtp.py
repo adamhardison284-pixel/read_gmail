c0c69bbba9e9174ec6386b9109f63459b4c75438
adamhardison284-pixel <adamhardison284@gmail.com>
2025-11-07 23:22:38 +0100

    Update test_smtp.py

--- test_smtp.py
+++ test_smtp.py
@@ -35,7 +35,7 @@ sender_email = 'let@a1186466.xsph.ru'
 password = 'Arbinaji1987$'
 smtp.a1185346.xsph.ru
 """
-iddd = "a1187527"
+iddd = "a1187440"
 receiver_email = "bryan.pell@hotmail.com"
 receiver_email = "adamoyler2705cc@web.de"
 #receiver_email = "arbi.naji@gmail.com"
